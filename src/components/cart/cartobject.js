d4b0ebeb89ef7a180b2213f5e47e26752092f9cb
SpaceCowboy20 <housseine18@gmail.com>
2022-02-10 00:09:44 +0100

    cart object almost done

--- src/components/cart/cartobject.js
+++ src/components/cart/cartobject.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import "./cartobject.css";
 
 class Cartobject extends Component {
   render() {
     const productData = this.props.productData;
     return (
       <div className="cartobject">
-        <img src={productData.image} alt="" className="img" />
-        <h2>{productData.title}</h2>
-        <p>{productData.description}</p>
+        <div className="cart-img-box">
+          <img src={productData.image} alt="" className="cart-img" />
+        </div>
+        <div className="cart-text-box">
+          <h2>{productData.title}</h2>
+          <p>{productData.description}</p>
+        </div>
       </div>
     );
   }
