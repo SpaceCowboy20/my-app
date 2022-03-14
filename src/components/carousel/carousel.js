a1a1c36cd406145724db7ddab422547ee4500e1f
SpaceCowboy20 <housseine18@gmail.com>
2022-03-14 16:51:22 +0100

    carousel2

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,91 +1,33 @@
-import { Carousel } from "react-carousel-minimal";
+import React from "react";
+import Carousel from "react-gallery-carousel";
+import "react-gallery-carousel/dist/index.css";
 
-function Carousell() {
-  const data = [
+const App = () => {
+  const images = [
     {
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
-      caption: "San Francisco",
+      src: "https://animecorner.me/wp-content/uploads/2022/01/roronoza-zoro-statue-in-japan.jpg",
     },
     {
-      image:
-        "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
-      caption: "Scotland",
+      src: "https://www.comicnity.com/wp-content/uploads/2021/05/Hisoka-Morrow.jpg?ezimgfmt=ng%3Awebp%2Fngcb6%2Frs%3Adevice%2Frscb6-2",
     },
-    {
-      image:
-        "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
-      caption: "Darjeeling",
-    },
-    {
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
-      caption: "San Francisco",
-    },
-    {
-      image:
-        "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-      caption: "Scotland",
-    },
-    {
-      image:
-        "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
-      caption: "Darjeeling",
-    },
-    {
-      image:
-        "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
-      caption: "San Francisco",
-    },
-    {
-      image:
-        "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
-      caption: "Scotland",
-    },
-    {
-      image:
-        "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
-      caption: "Darjeeling",
-    },
+    { src: "https://i.ibb.co/KDdD5QC/vihqjj26.png" },
+    { src: "https://animehunch.com/wp-content/uploads/2021/06/Eren-AOT.jpg" },
   ];
 
-  const captionStyle = {
-    fontSize: "2em",
-    fontWeight: "bold",
-  };
-  const slideNumberStyle = {
-    fontSize: "20px",
-    fontWeight: "bold",
-  };
   return (
-    <div className="App">
-      <Carousel
-        data={data}
-        time={2000}
-        width="650px"
-        height="400px"
-        captionStyle={captionStyle}
-        radius="10px"
-        slideNumber={true}
-        slideNumberStyle={slideNumberStyle}
-        captionPosition="bottom"
-        automatic={true}
-        dots={true}
-        pauseIconColor="white"
-        pauseIconSize="40px"
-        slideBackgroundColor="darkgrey"
-        slideImageFit="cover"
-        thumbnails={true}
-        thumbnailWidth="100px"
-        style={{
-          textAlign: "center",
-          maxWidth: "850px",
-          maxHeight: "500px",
-          margin: "40px auto",
-        }}
-      />
-    </div>
+    <Carousel
+      images={images}
+      isAutoPlaying={true}
+      autoPlayInterval={3000}
+      widgetsHasShadow={true}
+      hasIndexBoard={false}
+      hasDotButtons={"bottomCenter"}
+      hasSizeButton={false}
+      hasMediaButton={false}
+      hasThumbnails={false}
+      style={{ height: "30rem", width: "45rem" }}
+    />
   );
-}
+};
 
-export default Carousell;
+export default App;
